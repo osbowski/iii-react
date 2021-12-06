ddb3275955bcfc5a98e425a3cb79a908477377c6
osbowski <osobiesam@gmail.com>
2021-12-06 12:17:16 +0100

    install axios

--- src/components/QuoteComponent.tsx
+++ src/components/QuoteComponent.tsx
@@ -1,16 +1,47 @@
-const QuoteComponent = () => {
+import { useEffect, useState } from "react";
 
+const QuoteComponent = () => {
+  interface quoteInterface {
+    quote: string;
+    author: string;
+  }
+
+  const [quote, setQuote] = useState<quoteInterface | undefined>();
+  const [lastQuote, setLastQuote] = useState<quoteInterface | undefined>();
+
+  const getNewQuote = async () => {
+    if (quote !== undefined) {
+      setLastQuote(quote);
+    }
+    const data = await getRandomQuote();
+    const randomQuote = await data;
+    setQuote(randomQuote);
+  };
+
+  const getLastQuote = () => {
+    setQuote(lastQuote);
+    setLastQuote(undefined)
+  };
+
+  useEffect(() => {
+    getNewQuote();
+  }, []);
 
   return (
     <div className="section-container">
       <h2>Quote for you</h2>
+
+      {quote === undefined ? (
+        <p>Something gone wrong. Try again later</p>
+      ) : (
         <div className="quote-container">
-          <p className="quote">"THIS IS QUOTE"</p>
-          <p className="author">Author of quote</p>
+          <p className="quote">{quote.quote}</p>
+          <p className="author">{quote.author}</p>
         </div>
+      )}
       <div>
-        <button>Get new quote</button>
-        <button>Back to last quote</button>
+        <button onClick={getNewQuote}>Get new quote</button>
+        <button className="outline" onClick={getLastQuote} disabled={lastQuote===undefined}>Back to last quote</button>
       </div>
     </div>
   );
